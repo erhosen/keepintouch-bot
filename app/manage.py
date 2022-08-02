ce6ec1504f3683c8e28bf920e230b691a4d52376
Vladimir Vyazovetskov <erhosen@gmail.com>
2022-08-02 18:24:28 +0300

    Deploment: start.

--- app/manage.py
+++ app/manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+import json
 import os
 import sys
+from typing import Optional
 
 
 def main():
@@ -18,14 +20,25 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-def handle(*args, **options):
+def handle(event: Optional[dict], context: Optional[dict]):
     """
     Handle lambda call
     """
     import django
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keepintouch.settings')
     django.setup()
-    pass
+
+    body = json.loads(event['body'])  # type: ignore
+
+    from tgbot.dispatcher import process_telegram_event
+    process_telegram_event(body)
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps({"ok": "POST request processed"}, ensure_ascii=False),
+        'isBase64Encoded': False,
+    }
 
 
 if __name__ == '__main__':
