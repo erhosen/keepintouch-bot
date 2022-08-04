5325da3b28e2e01fa75ec90cf37636194145a4df
Vladimir Vyazovetskov <erhosen@gmail.com>
2022-08-04 13:19:57 +0300

    Tests.

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,14 +2,57 @@ import pytest
 from django.conf import settings
 
 
-class YCContext:
-    def __init__(self):
-        self.token = {"access_token": "CggVAgAAABoBMRKABHGgpZ......", "expires_in": 42299, "token_type": "Bearer"}
+@pytest.fixture(autouse=True)
+def enable_db_access(db):
+    pass
 
 
 @pytest.fixture
-def yc_context():
-    return YCContext()
+def mbot():
+    class Bot:
+        def __init__(self):
+            self.args = []
+            self.kwargs = {}
+            self.defaults = None
+            super().__init__()
+
+        def send_message(self, *args, **kwargs):
+            self.args.append(args)
+            self.kwargs.update(kwargs)
+
+        @property
+        def text(self):
+            return self.kwargs.get('text')
+
+    return Bot()
+
+
+@pytest.fixture
+def start_command_json():
+    return {
+        "update_id": 999999999,
+        "message": {
+            "message_id": 999999999,
+            "from": {
+                "id": 999999999,
+                "is_bot": False,
+                "first_name": "John",
+                "last_name": "Doe",
+                "username": "jdoe",
+                "language_code": "en-US",
+            },
+            "chat": {
+                "id": 999999999,
+                "first_name": "John",
+                "last_name": "Doe",
+                "username": "jdoe",
+                "type": "private",
+            },
+            "date": 1546300800,
+            "text": "/start",
+            "entities": [{"offset": 0, "length": 6, "type": "bot_command"}],
+        },
+    }
 
 
 def pytest_configure():
@@ -19,12 +62,13 @@ def pytest_configure():
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': ':memory:',
-                'INSTALLED_APPS': [
-                    'django.contrib.contenttypes',
-                    'django.contrib.messages',
-                    'tgbot.apps.TgbotConfig',
-                ],
             }
         },
+        INSTALLED_APPS=[
+            'tgbot.apps.TgbotConfig',
+            'django.contrib.contenttypes',
+            'django.contrib.messages',
+        ],
+        TELEGRAM_TOKEN='123456789:AABBCCDDEEFFgghhiiijjkkllmmnnoopp',
         DEBUG=True,
     )
