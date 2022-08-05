3a2915b4de31cc09bdf9bac74f3be21221c53d32
Vladimir Vyazovetskov <erhosen@gmail.com>
2022-08-05 18:32:48 +0300

    Fix tests.

--- app/tests/test_notification.py
+++ app/tests/test_notification.py
@@ -7,12 +7,13 @@ from tgbot.core import Group
 from tgbot.handlers.notification import callback_keepintouch, send_notification_message
 
 
+@freeze_time('2022-08-01')
 def test_send_notification_message(mbot, contact):
     send_notification_message(mbot, contact.user, contact)
 
     assert mbot.text == (
         "💬 It's time to write a few words to [Jane](https://t.me/+78888888888) \n\n"
-        "Last time you contacted *24 days ago* (List A)\n"
+        "Last time you contacted *21 days ago* (List A)\n"
     )
     assert mbot.reply_markup.inline_keyboard[0][0].text == 'Ok'
     assert mbot.reply_markup.inline_keyboard[0][1].text == 'Tomorrow'
